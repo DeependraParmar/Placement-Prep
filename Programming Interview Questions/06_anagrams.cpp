97faf393d1a037647728d17af9f10e99bc07d9c7
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-18 18:59:11 +0530

    Anagrams

--- Programming Interview Questions/06_anagrams.cpp
+++ Programming Interview Questions/06_anagrams.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 using namespace std;
 
+bool checkAnagram(string str1, string str2){
+    bool isAnagram = true;
+
+    if(str1.length() != str2.length()){
+        return false;
+    }
+    else{
+        for(int i=0; i<str1.length(); i++){
+        char ch1 = str1[i];
+        bool isFound = false;
+
+            for(int j=0; j<str1.length(); j++){
+                char ch2 = str2[j];
+
+                if(ch1 == ch2){
+                    isFound = true;
+                    str2[j] == '@'; // marking the character as second string as marked/paired.
+                    continue;    
+                }
+            }
+
+            if(!isFound){
+                isAnagram = false;
+                break;
+            }
+        }
+    }
+
+    return isAnagram;
+}
 int main(){
     // Two strings are said to be anagrams if by rearranging any one of them, gives the second string. Basically, the character and the their occurences in the string is equal.
     // For eg: cinema and iceman.
@@ -14,29 +44,10 @@ int main(){
 
     // brute force: for every single character in first string, search for the exact character in the second one and if found, change the character as marked.
 
-    // simple approach: sort both of the strings and then, check for every character. 
+    // optimal approach: sort both of the strings and then, check for every character.
 
-    bool isAnagram = true;
-    for(int i=0; i<str1.length(); i++){
-        char ch1 = str1[i];
-        bool isFound = false;
-
-        for(int j=0; j<str1.length(); j++){
-            char ch2 = str2[j];
-
-            if(ch1 == ch2){
-                isFound = true;
-                str2[j] == '@';
-                continue;    
-            }
-        }
-
-        if(!isFound){
-            isAnagram = false;
-            break;
-        }
-    }
-
+    
+    bool isAnagram = checkAnagram(str1, str2);
     cout << endl;
 
     if(isAnagram){
