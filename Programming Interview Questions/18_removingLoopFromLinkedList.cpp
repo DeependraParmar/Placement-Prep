d0aeb7dece2ad28a9ff174cfe48a374b59b0b45b
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-07-20 11:13:30 +0530

    Removing loop from linked list: Floyd's cycle detection algorithm

---
+++ Programming Interview Questions/18_removingLoopFromLinkedList.cpp
@@ -0,0 +1,57 @@
+// Solve the problem at GeeksForGeeks: https://www.geeksforgeeks.org/problems/remove-loop-in-linked-list/1
+
+// Amazing problem: approach: 
+/*
+    1. Finding the point of intersection using Tortoise and hare algorithm.
+    2. Finding the number of nodes in the loop.
+    3. Reaching the same point of intersection using fast
+    4. Starting slow from head and fast from POI. Moving them forward until fast reaches the last node of the loop
+    5. Making fast -> next == NULL. Hence removing the loop.
+*/
+
+/*
+class Solution
+{
+    public:
+    void removeLoop(Node* head){
+        if(head == NULL)
+            return;
+
+        // Tortoise and hare algorithm for intersection point
+        Node* slow = head;
+        Node* fast = head -> next;
+
+        while(fast != slow){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+
+            if(!fast || !fast -> next)
+                return;
+        }
+
+        // finding the no of nodes in the loop
+
+        int size = 1;
+        fast = fast -> next;
+
+        while(fast != slow){
+            size++;
+            fast = fast -> next;
+        }
+
+        slow = head;
+        fast = head;
+
+        for(int i=0; i<size-1; i++)
+            fast = fast -> next;
+
+
+        while(fast -> next != slow){
+            fast = fast -> next;
+            slow = slow -> next;
+        }
+
+        fast -> next = NULL;
+    }
+};
+*/
